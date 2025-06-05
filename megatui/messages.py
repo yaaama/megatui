298e8424b05ed239d46e2ced46e835c88c4ccdc9
Sau P <sau@yaaama.xyz>
2025-06-05 09:23:40 +0100

    Messages file added for global messages

---
+++ megatui/messages.py
@@ -0,0 +1,14 @@
+# Global Messages
+#
+from textual.message import Message
+from rich.text import Text
+
+
+class UpdateStatusMsg(Message):
+    """
+    Message for a widget to update status bar.
+    """
+
+    def __init__(self, message : str | Text) -> None:
+        super().__init__()
+        self.message = message
