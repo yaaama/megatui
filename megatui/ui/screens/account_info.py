465acf0191aacc4d4f85fb4ceb4b12bb519b9b87
Sau P <sau@yaaama.xyz>
2025-07-04 10:50:01 +0100

    wip: Account info screen

--- megatui/ui/screens/account_info.py
+++ megatui/ui/screens/account_info.py
@@ -3,14 +3,30 @@
 from typing import override
 
 from textual.app import ComposeResult
+from textual.binding import Binding
 from textual.containers import Vertical, VerticalScroll
 from textual.screen import ModalScreen
 from textual.widgets import Header, Label, RichLog
+
+import megatui.mega.megacmd as m
 
 
 # TODO Finish this
 class AccountInformationScreen(ModalScreen[None]):
     """Screen to display cloud storage account information."""
+
+    BINDINGS = (
+        Binding(key="escape", action="app.pop_screen", show=False, priority=True),
+    )
+
+    def __init__(self, df_data: m.StorageOverview, pwd, whoami, mount, speedlimits):
+        super().__init__()
+
+        self._df_data: m.StorageOverview = df_data
+        self._pwd: str = pwd
+        self._whoami: str = whoami
+        self._mount: str = mount
+        self._speedlimits: str = speedlimits
 
     @override
     def compose(self) -> ComposeResult:
@@ -18,5 +34,4 @@ class AccountInformationScreen(ModalScreen[None]):
             vs.can_focus = False
             yield Header(show_clock=False, id="account-info-header")
             yield Label("This is a helpful screen of information.")
-
         pass
