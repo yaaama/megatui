a8040b86d8613827d318ec01dbe9d8a7bc51ee69
Sau P <sau@yaaama.xyz>
2025-08-06 20:41:20 +0100

    feat: Upload files screen
    - Selected files will display indicator
    - Key binding 'o' to open the screen

--- src/megatui/ui/file_tree.py
+++ src/megatui/ui/file_tree.py
@@ -1,29 +1,48 @@
 # File tree
 from pathlib import Path
-from typing import override
+from sre_parse import SUBPATTERN
+from string import Template
+from tokenize import String
+from typing import ClassVar, assert_type, override
 
+from rich.text import Text, TextType
+from textual import on
 from textual.app import ComposeResult
-from textual.binding import Binding
+from textual.binding import Binding, BindingType
+from textual.containers import Vertical
+from textual.content import Content
 from textual.screen import ModalScreen
-from textual.widgets import DirectoryTree, Static
+from textual.widgets import DirectoryTree, Label, Static, Tree
+from textual.widgets._directory_tree import DirEntry
+from textual.widgets.tree import TreeNode
 
 
-class FileTreeScreen(ModalScreen[None]):
+class UploadFilesModal(ModalScreen[str | None]):
+    BINDINGS: ClassVar[list[BindingType]] = [
+        Binding(key="escape,q", action="app.pop_screen", show=False, priority=True),
+        Binding(key="enter", action="file_select", show=True),
+    ]
+
     @override
     def compose(self) -> ComposeResult:
-        yield LocalSystemFileTree()
+        with Vertical():
+            yield Label("Select file to upload", id="uploadfiles-heading")
+            yield LocalSystemFileTree(
+                id="filetree",
+                starting_path="/home/aayush/downloads",
+            )
 
 
-class LocalSystemFileTree(Static):
+class LocalSystemFileTree(DirectoryTree):
     BINDINGS = [
         Binding(
-            "shift+left",
+            "ctrl+h",
             "cursor_parent",
             "Cursor to parent",
             show=False,
         ),
         Binding(
-            "shift+right",
+            "ctrl+l",
             "cursor_parent_next_sibling",
             "Cursor to next ancestor",
             show=False,
@@ -52,7 +71,49 @@ class LocalSystemFileTree(Static):
         Binding("j", "cursor_down", "Cursor Down", show=False),
     ]
 
-    @override
-    def compose(self) -> ComposeResult:
-        home: Path = Path().home()
-        yield DirectoryTree(path=home)
+    COMPONENT_CLASSES = {
+        "node-selected",
+    }
+
+    _selected_items: set[Path] = set()
+
+    def _toggle_node_selection(self, node: TreeNode[DirEntry]):
+        if not node:
+            return
+
+        # Name of the node
+        name = node.label
+
+        # Data stored in node
+        node_data = node.data
+        if not node_data:
+            self.log.error("No data associated with node.")
+            return node
+
+        fpath = node_data.path
+
+        if fpath in self._selected_items:
+            # remove
+            self._selected_items.remove(fpath)
+            label = Text(str(name))
+
+        else:
+            # Template for when item is selected
+            label_tmpl = Template("[b][i][red]$path[/][/][/]")
+            label = Text.from_markup(label_tmpl.substitute(path=name))
+            self._selected_items.add(fpath)
+
+        node.set_label(label)
+        return node
+
+    def on_directory_tree_file_selected(self, msg: DirectoryTree.DirectorySelected):
+        self._toggle_node_selection(msg.node)
+
+    def on_directory_tree_directory_selected(
+        self, msg: DirectoryTree.DirectorySelected
+    ):
+        self._toggle_node_selection(msg.node)
+
+    def __init__(self, starting_path: str, **kwargs):
+        super().__init__(starting_path)
+        self.auto_expand = False
