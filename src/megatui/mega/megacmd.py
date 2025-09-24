04ee3590ad7a6c6415070ba23cd82b150292cbb2
Sau P <sau@yaaama.xyz>
2025-09-24 20:21:28 +0100

    megacmd: logger tweaks

--- src/megatui/mega/megacmd.py
+++ src/megatui/mega/megacmd.py
@@ -13,15 +13,18 @@ from typing import NamedTuple, TypedDict, override
 
 logging.basicConfig(
     filename="megacmd.log",
-    filemode="a",
+    filemode="w",
     level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s",
+    # format="%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s",
+    format="%(asctime)s - %(levelname)s - %(funcName)s :: %(message)s",
 )
 logger = logging.getLogger(__name__)
 # Get the logger for the 'asyncio' library and set its level to WARNING.
 # This will hide all INFO and DEBUG messages from asyncio.
 logging.getLogger("asyncio").setLevel(logging.WARNING)
-logger.debug("This is a debug message.")
+logger.info(f"================")
+logger.info(f"'megacmd' LOADED.")
+logger.info(f"================")
 
 MEGA_COMMANDS_ALL: set[str] = {
     "attr",
