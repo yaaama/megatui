2a0c9377b1aac63d676ab5b09049c53b6a5685aa
Sau P <sau@yaaama.xyz>
2025-10-07 01:15:12 +0100

    update dirlocal vars

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "MEGAcmd MegaTUI"))))
+((nil . ((jinx-dir-local-words . "MEGAcmd MegaTUI")))
+ (python-base-mode . ((tab-width 4))))
