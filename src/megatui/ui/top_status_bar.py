cf7be4fe3699cf4fef6fe3e289f1d95ce45e884d
Sau P <sau@yaaama.xyz>
2025-11-06 09:28:23 +0000

    refactor(top_status_bar): Use textual.getters

--- src/megatui/ui/top_status_bar.py
+++ src/megatui/ui/top_status_bar.py
@@ -1,5 +1,6 @@
 from typing import ClassVar, LiteralString, override
 
+import textual.getters as getters
 from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.reactive import var
@@ -14,8 +15,10 @@ class TopStatusBar(Horizontal):
     path: var[str] = var("")
     status_msg: var[str] = var("")
 
-    DEFAULT_CSS = """
-    """
+    path_label = getters.child_by_id("bar--path-str", Label)
+    status_msg_label = getters.child_by_id("bar--msg", Label)
+
+    DEFAULT_CSS = """ """
 
     @override
     def compose(self) -> ComposeResult:
@@ -29,22 +32,20 @@ class TopStatusBar(Horizontal):
 
     def watch_path(self, new_path: str) -> None:
         """Called when self.path is modified."""
-        path_label = self.query_one(f"#{self.PATH_STR_ID}", Label)
-        path_label.update(f"'{new_path}'")
+        self.path_label.update(f"'{new_path}'")
 
     def watch_status_msg(self, new_status_msg: str) -> None:
         """Called when self.status_msg is modified."""
-        status_msg_label = self.query_one(f"#{self.STATUS_MSG_ID}", Label)
-        status_msg_label.update(f"[b]{new_status_msg}[/b]")
+        self.status_msg_label.update(f"[b]{new_status_msg}[/b]")
 
     def clear_status_msg(self) -> None:
-        status_msg_label = self.query_one(f"#{self.STATUS_MSG_ID}", Label)
-        status_msg_label.update()
+        """Clear status message from top bar."""
+        self.status_msg_label.update()
 
     def signal_empty_dir(self) -> None:
-        status_msg_label = self.query_one(f"#{self.STATUS_MSG_ID}", Label)
-        status_msg_label.update("[b][red]Empty directory[/b][/red]")
+        """Signal to the user that the directory is empty."""
+        self.status_msg_label.update("[b][red]Empty directory[/b][/red]")
 
     def signal_error(self, err_msg: str):
-        status_msg_label = self.query_one(f"#{self.STATUS_MSG_ID}", Label)
-        status_msg_label.update(f"[b][red][reverse]{err_msg}[/][/][/]")
+        """Signal an error to the user."""
+        self.status_msg_label.update(f"[b][red][reverse]{err_msg}[/][/][/]")
